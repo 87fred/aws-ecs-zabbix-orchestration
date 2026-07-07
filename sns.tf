d9494f3e8872ee00fdb254bef064b4633991f6ef
fred <frederico.almeidamoreira@gmail.com>
2026-07-06 23:02:05 -0300

    fix:ajuste no sns.tf - monitoring.tf

--- sns.tf
+++ sns.tf
@@ -7,7 +7,7 @@ resource "aws_sns_topic" "sns_alerts" {
 
 #2. Inscreve o seu e-mail no canal de notificação (SNS Topic) para receber alertas
 resource "aws_sns_topic_subscription" "sns_email_subscription" {
-    topic_arn = aws_sns_topic.sns_alerts.arn
-    protocol  = "email"
-    endpoint  = "email@email.com" # Substitua pelo seu e-mail
+  topic_arn = aws_sns_topic.sns_alerts.arn
+  protocol  = "email"
+  endpoint  = "email@email.com" # Substitua pelo seu e-mail
 }
